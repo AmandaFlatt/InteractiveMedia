dd005ddc3751b27f085e09ed6727fa9c5eec93c3
AmandaFlatt <60753821+AmandaFlatt@users.noreply.github.com>
2020-03-03 22:13:36 -0500

    Update Moving.js

--- Moving.js
+++ Moving.js
@@ -1,9 +1,62 @@
+function myMove(speed,topMv,botMv,leftMv,rightMv) {
+    var elem = document.getElementById('mole');
+    var pos = 50;
+    var id = setInterval(frame, speed);
+    function frame() {
+        if (pos == 350){
+            clearInterval(id);
+            
+            
+        }else{
+            pos++;
+            if(topMv){
+                elem.style.top=pos+'px';
+                
+            }
+            if(leftMv){
+                elem.style.left=pos+'px';
+            }
+            if(botMv){
+                elem.style.top=(350-pos)+'px';
+            
+        }
+           if(rightMv){
+                elem.style.left=(350-pos)+'px';
+    }
+    
+    
+}
+    }
+}
+
+var counter = 0;
 d3.select("#mole")
   .on ("click", function()
      {
-    d3.select("body")
-      .append("p")
-       .text("points =1")
+    
+    if(counter == 0){
+        console.log('its doing this one')
+    myMove(3,true,false,true,true)
+    }
+    else if(counter == 1)
+        {
+            console.log('its doing 2')
+            myMove(3,false,true,false,true)
+        }
+    else if(counter == 2)
+        {
+            console.log('its doing 3')
+            myMove(3,true,false,false,true)
+        }
+    else
+        {
+            console.log('done')
+            d3.select("body")
+            .append("p")
+            .text("done, good job")
+        }
+    counter = counter+1
+    console.log(counter)
 
     
 })
